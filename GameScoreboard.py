6b1868bbf0e3d0fa351e88bd135ded84d03710ff
FocusWaiver <113658378+FocusWaiver@users.noreply.github.com>
2025-05-16 14:08:18 -0400

    Update GameScoreboard.py
    finished continue option. No file management yet

--- GameScoreboard.py
+++ GameScoreboard.py
@@ -27,20 +27,60 @@ def FileError(playfile):
         return False
         
 def newGame():
+    roundzero = []
     print("Who is playing?")
     players = input()
     playlist = players.split(",")
     #File name
     print("What would you like to name this file?")
     playfile = input()
-    playfilefull = path+playfile+".txt"
+    playfiletxt = playfile+".txt"
     mistakes = FileError(playfile)
     if mistakes:
         playfile = input()
-    #create new file with user name
+    #create new file with name
+    #open path+playfiletxt
     #write playlist to file
-    print("Great, your save file is called ", playfilefull)
+    for each in playlist:
+        entry = each+"-0"
+        roundzero.append(entry)
+    #upload roundzero
+    print("Great, your save file is called ", playfiletxt)
     print("Have Fun!")
+    
+def contGame():
+    #!
+    #change this for less typing between rounds
+    contfilename = "game.txt"
+    #!
+    
+    #testing list
+    content = ("n,k,m","['n-0','k-0','m-0']","['n-2','k-6','m-1']","['n-22','k-46','m-10']")
+    updatelist = []
+    #contf = open(game.txt)
+    #content = contf.readlines()
+    playerlist = content[0]
+    playsplit = playerlist.split(",")
+    currentscore = content[-1]
+    #make a function to use multiple times?
+    junk = ["[","]","'"]
+    for j in junk:
+        currentscore = currentscore.replace(j,"")
+    print(currentscore)
+    currsplit = currentscore.split(",")
+    for i in currsplit:
+        separate = i.split("-")
+        player = separate[0]
+        score = int(separate[1])
+        print("what was ",player, "'s score this round?")
+        newscore = input()
+        #try int
+        newint = int(newscore)
+        addition = str(score+newint)
+        updatelist.append(player+"-"+addition)
+    print("The scores for round ", len(content)-1, "are", updatelist)
+    #upload
+        
     
     
 # Welcome
@@ -48,31 +88,12 @@ print("Welcome!")
 #New or continuing game
 print("0 for a new game, 1 to continue a past game")
 #gameAns = input()
-gameAns = "0"
+gameAns = "1"
 if gameAns == "0":
     newGame()
-    
-    
 elif gameAns == "1":
-    print(1)
+    contGame()
 else:
     print("Sorry, invalid input.")
-#Grab cache file with information
-
-# If new, Prompt for team/player names
-#prompt name of file save
-#will need try() and loop to check name won’t break anything
-#store information/write to cache or make new file? Format it so you can look back
-
-#if continue, split teams/player to retrieve scores, Count /n for round number
-#print game information, active score, round number
-#prompt iteration of score entry
-#calculate new score
-#return updated score
-#save new information
 
 #pickup a different saved game? Can python print folder contents?
-
-
-
-
