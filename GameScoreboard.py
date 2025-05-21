8ef8b38c129616b1ec63c1e051a4008779c72dd2
FocusWaiver <113658378+FocusWaiver@users.noreply.github.com>
2025-05-21 11:33:58 -0400

    Update GameScoreboard.py
    Added README for instructions on what to change if running on a new device. Added more descriptive pseudocode.

--- GameScoreboard.py
+++ GameScoreboard.py
@@ -2,13 +2,21 @@
 # To provide round calculations for keeping score
 #  5/14/25 - fw
 #
+# README
+# Two hard code sections that need to be edited.
+# (1) Find path of directory you wish new game save files to be stored
+# (2) Update desired game file after creating a new save to avoid having to type in...
+# desired save file name between every round.
+#
 #
 
-#path for files
+#path for files, Edit (1)
 path = "/storage/emulated/0/Documents/Pydroid3/mobilePython/"
 import os
+#Can use the following command to find current working directory
 #print(os.getcwd())
 
+#used to remove storage artifacts from saved strings
 def dejunk(dirty):
     junk = ["[","]","'","(",")"," "]
     for j in junk:
@@ -16,9 +24,8 @@ def dejunk(dirty):
     	dirty = clean
     return clean
 	
-
+#Verifying name of save file input by user is valid/does not already exist
 def FileError(playfile):
-    #Can this be the file name?
     symbols = "{}|[]!@#$%^&*()-_=+;:?.,<>"
     for sym in symbols:
         symfind = playfile.find(sym)
@@ -40,21 +47,21 @@ def newGame():
     print("Who is playing?")
     players = input()
     playlist = players.split(",")
-    #File name
+    #File name input by user
     print("What would you like to name this file?")
     playfile = input()
     playfiletxt = playfile+".txt"
     mistakes = FileError(playfile)
     if mistakes:
         playfile = input()
-    #create new file with name
+    #create new file with input name
     playf= open(path+playfiletxt, "x")
     playf.close()
     #write playlist to file
     for each in playlist:
         entry = each+"-0"
         roundzero.append(entry)
-    #upload roundzero
+    #upload roundzero for consistancy in contGame() data retrieval
     playrz = open(path+playfiletxt, "w")
     playrz.write(str(playlist)+"\n")
     playrz.write(str(roundzero))
@@ -64,12 +71,10 @@ def newGame():
     
 def contGame():
     #!
-    #change this for less typing between rounds
-    contfilename = "GinGame51925https://youtu.be/K5KVEU3aaeQ?si=r2ifwliXevGaMVBV.txt"
+    #change this for less typing between rounds, Edit (2)
+    contfilename = "GinGame51925.txt"
     #!
     
-    #testing list
-    #content = ("n,k,m","['n-0','k-0','m-0']","['n-2','k-6','m-1']","['n-22','k-46','m-10']")
     updatelist = []
     cplayerlist = []
     contf = open(contfilename,"r")
@@ -87,30 +92,33 @@ def contGame():
         score = int(scorec)
         print("what was ",playerc, "'s score this round?")
         newscore = input()
-        #try int
+        #Convert stored score from string to integer for calculations
+        #!INPUT TRY for Errors!
         newint = int(newscore)
         addition = str(score+newint)
         updatelist.append(playerc+"-"+addition)
     print("The scores for round ", len(content)-1, "are", updatelist)
-    #upload
+    #upload latest score to save file
     roundf = open(contfilename,"a")
     roundf.write("\n"+str(updatelist))
     roundf.close()
         
     
     
-# Welcome
+# Initial interface/Welcome
 print("Welcome!")
 #New or continuing game
 print("0 for a new game, 1 to continue a past game")
 gameAns = input()
-#gameAns = "3"
 if gameAns == "0":
     newGame()
 elif gameAns == "1":
     contGame()
 else:
     print("Sorry, invalid input.")
+    
+#TODO
+# Add Try for score input
 
-#pickup a different saved game? Can python print folder contents?
+
 
